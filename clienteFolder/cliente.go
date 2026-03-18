f7b2e01d4745d792305f8b09410a56117711d109
LuanRAr <luanlra002@gmail.com>
2026-03-17 22:09:09 -0300

    x

---
+++ clienteFolder/cliente.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+)
+
+func main(){
+	conectServer, error1 := net.Dial("tcp", ":8080")
+	if error1 != nil{
+		fmt.Println(error1)
+		return
+	}
+	
+	fmt.Println("Se conectando no server...")
+	defer conectServer.Close()
+	
+	mensagem := "Hello Server!\n"
+
+	fmt.Fprintf(conectServer, mensagem)
+
+	fmt.Println("mensagem Enviada")
+
+	//ouvir resposta
+	res, error4 := bufio.NewReader(conectServer).ReadString('\n')
+	if error4 != nil{
+		fmt.Println(error4)
+	}
+	fmt.Println(res)
+}
