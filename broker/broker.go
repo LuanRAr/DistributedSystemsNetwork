384a98fd4cb6a29fa4b08d7e8dd15caa33a03a31
LuanRAr <luanlra002@gmail.com>
2026-03-24 22:48:56 -0300

    tratamento de escolha do cliente feita

--- broker/broker.go
+++ broker/broker.go
@@ -15,41 +15,112 @@ type Menu struct {
 }
 
 func main(){
+	serverTCP()
+
+}
+
+//UDP
+type Coords struct{
+	Latitude float32  
+	Longitude float32 
+}
+
+type Object struct{
+	Name string
+	Coordinates []Coords
+	Doors string
+}
+
+//Servidor TCP----------------------------------------------------------------------------
+func serverTCP(){
 	server, err := net.Listen("tcp", ":4041")
 	if err != nil{
 		fmt.Println("Erro: ", err)
 		return
 	}
 
+	defer server.Close()
+
 	for {
 		conn, err := server.Accept()
 		if err != nil{
 			fmt.Println("Erro: ", err)
-			return
+			continue
 		}
 
-		defer conn.Close()
+		go handleConnection(conn)
+	}
 
-		menu := Menu{
-			Texto: "1. Listar Objetos\n2. Trancar Objeto",
-		}
+}
 
-		//enviar menu pro cliente
-		encoder := json.NewEncoder(conn)
-		encoder.Encode(menu)
+func handleConnection(conn net.Conn){
+	defer conn.Close()
 
-		//receberr input do cliente
-		decoder := json.NewDecoder(conn)
-		var input UserInput
-		err2 := decoder.Decode(&input)
-		if err2 != nil {
-			fmt.Println("Erro ao ler resposta:", err2)
-			return
-		}
+	fmt.Println("Nova conexão de ", conn.RemoteAddr())
 
-		fmt.Printf("Usuário escolheu: %d\n", input.Option)
+	menu := Menu{
+		Texto: "1. Listar Objetos\n2. Trancar Objeto",
+	}
+
+	//enviar menu pro cliente
+	encoder := json.NewEncoder(conn)
+	encoder.Encode(menu)
+
+	//receber input do cliente
+	decoder := json.NewDecoder(conn)
+
+	var input UserInput
+
+	err2 := decoder.Decode(&input)
+	if err2 != nil {
+		fmt.Println("Erro ao ler resposta:", err2)
+		return
+	}
+
+	switch input.Option {
+		case 1:
+			fmt.Println("Usuário escolheu 1")
+		case 2: 
+			fmt.Println("Usuário escolheu 2")
+		default:
+			fmt.Println("Default")
 
 	}
 
+}
 
+//Servidor UDP----------------------------------------------------------------------------
+func serverUDP(){
+	addr, err := net.ResolveUDPAddr("udp", ":8081")
+	if err != nil {
+		fmt.Println("Erro", err)
+		return
+	}
+
+	conn, err2 := net.ListenUDP("udp", addr)
+	if err != nil{
+		fmt.Println("Erro: ", err2)
+	}
+
+	defer conn.Close()
+
+	for {
+		//mensagem que usuario passou em pacote
+		buffer := make([]byte, 1024)
+
+		//ler os dados
+		n, _, err3 := conn.ReadFromUDP(buffer)
+		if err3 != nil{
+			fmt.Println("Erro: ", err3)
+			continue
+		}
+		
+		var sensor Object
+		err4 := json.Unmarshal(buffer[:n], &sensor)
+		if err4 != nil {
+			fmt.Println("Erro: ", err4)
+			continue
+		}
+
+	}
 }
